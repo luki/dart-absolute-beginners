fec48d5ee6a6fddf5e31fe4a56061486e9b19907
Luke Muller <lamuller@protonmail.com>
2016-11-05 14:47:26 -0700

    Add FancyHelloWorld

---
+++ helloworldfancy/web/main.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2016, Luke Muller. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
+
+void main() {
+  querySelector("#button"). onClick.listen(sayHello);
+}
+
+void sayHello(MouseEvent event) {
+  querySelector("#name").text = (querySelector("#name_box") as InputElement).value;
+  (querySelector("#name_box") as InputElement).value = "";
+}
